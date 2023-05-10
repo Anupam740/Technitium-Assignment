4b2e2669b1c803411bc1171ce8093d962dad1ec9
Anupam740 <anupamkumar740@gmail.com>
2023-05-10 11:07:16 +0530

    8th-may-2023-assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-
-// import Axiosdogimgapi from './components/Axiosdogimgapi';
-import Axiosotp from './components/Axios-OTP_generate/Axiosotp'
+import Media from "./components/Media QUERY-Assignnment/Media"
+// import Media from './../technitium-react/src/components/Media QUERY-Assignnment/Media';
 
 const App = () => {
   return (
     <div>
-      <Axiosotp/>
+      <Media/>
     </div>
   )
 }
